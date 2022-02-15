396cbded812afc95ecf31c3eedf7577a96471a39
saji <champ189@umn.edu>
2022-02-15 15:04:13 -0600

    finish first pass of polly library

--- lib/polly/polly.h
+++ lib/polly/polly.h
@@ -8,6 +8,7 @@
 #include <sys/epoll.h>
 #include <memory>
 #include <vector>
+#include <map>
 #include <system_error>
 namespace polly {
 
@@ -19,64 +20,91 @@ namespace polly {
 
 // So what's the solution? The best way I can think of is to use shared pointers and a map of
 // weak_ptr stored in the Epoll class. Then, when it comes to adding a thing (Socket), we take a
-// weak pointer to it, add the weak pointer to our vector, *and then store the index with the
+// weak pointer to it, add the weak pointer to our map, *and then store the key with the
 // epoll_event*. This way, when it comes time to reconstruct, we can middleman the returned
-// epoll_event and get the index out, and look up the index to get the weak_ptr, which we can easily
+// epoll_event and get the index out, and look up the key to get the weak_ptr, which we can easily
 // runtime-check using lock().
 
 // another issue is handling the return of epoll_wait, which returns events + data. we could do this
 // by making a struct inside the class. Then, the wait() call will return a vector of these structs,
-// which contains a weak_ptr (leave handling lock to caller) as well as a uint32_t for events.
-
-
-// Create a helper function to make epoll events.
-using epoll_event_p = std::unique_ptr<epoll_event>;
-
-// should not be needed, since the Epoll class will automatically construct
-// an epoll event. 
-epoll_event_p create_event(uint32_t events, epoll_data_t data) {
-	return std::make_unique<epoll_event>(events,data);
-}
-
+// which contains a weak_ptr (leave handling lock to caller) as well as a uint32_t for events. This
+// way makes it easier to handle the wait call.
 
 template<class T>
 class Epoll {
-
 	int epoll_fd = -1;
-	std::vector<std::weak_ptr<T>> lut;
+	// the lut contains a bunch of weak_ptrs to 
+	std::map<int, std::weak_ptr<T>> lut;
 	using event_result = struct {
 		std::weak_ptr<T> object;
 		uint32_t events;
 	};
 	public:
-	Epoll();
-	Epoll(int fd) : epoll_fd(fd) {};
 
-	~Epoll();
+	Epoll() {
+		epoll_fd = epoll_create1(EPOLL_CLOEXEC);
+	};
+	// I'm not sure why you'd want this, but okay.
+	Epoll(int fd) : epoll_fd(fd) {};
+	~Epoll(){ close(); };
 
 	// Closes the epoll file descriptor. Will be called automatically.
-	void close();
+	void close() {
+		close(epoll_fd);
+	};
 
 	void add_item(std::shared_ptr<T> item, uint32_t events) {
-		int index = lut.size(); // this is where the next addition will go.
+		int index = item->get_fd(); // this is where the next addition will go.
 		auto ev = std::make_unique<epoll_event>(events, index);
-		int result = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, item->get_fd(), ev);
+		int result = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, index, ev.get());
 		if (result == -1) {
 			throw std::system_error(errno, std::generic_category(), "epoll_ctl() failed");
 		}
 		// it didn't fail, so we add our new guy to the lut.
-		lut.emplace_back(item);
+		lut.emplace(index, item);
 	};
 
-	// trims the fat of the LUT (removes dead ptrs), and clears the epoll
-	void rebuild();
-
-	void add_items(std::vector<T> items, epoll_event_p ev);
+	// same as add_item, but for a vector of items. uses the same events list for all
+	void add_items(std::vector<std::shared_ptr<T>> items, uint32_t events) {
+		for (auto item : items) {
+			add_item(item, events);
+		}
+	}
 
 	// blocks until events occur.
-	std::vector<event_result> wait() {
+	std::vector<event_result> wait(int timeout) {
+		std::array<epoll_event, 100> events; // We can change this value later, but I doubt it will be needed.
+
+		int nevents = epoll_wait(epoll_fd, events.data(), 100, timeout);
+
+		if (nevents == -1) {
+			throw std::system_error(errno, std::generic_category(), "epoll_wait() failed");
+		}
+		// we have events! let's turn them into a list of event_results.
+		std::vector<event_result> results;
+		for(int i = 0; i < nevents; i++) {
+			auto evnt = events.at(i);
+			results.emplace(lut.at(evnt.data.fd), evnt.events);
+		}
+
+
+		return results;
+
 
 	}
+	// TODO: Modify item?
+	
+	// Removes an item from the epoll watch list.
+	void delete_item(std::shared_ptr<T> item) {
+		// clear it from lut
+		lut.erase(item->get_fd());
+
+		int result = epoll_ctl(epoll_fd, EPOLL_CTL_DEL, item->get_fd(), nullptr);
+
+		if (result == -1 && errno != ENOENT) { // if there's no entry, we don't care.
+			throw std::system_error(errno, std::generic_category(), "epoll_ctl() failed");
+		}
+	};
 };
 
 }
